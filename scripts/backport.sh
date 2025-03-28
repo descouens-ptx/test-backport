7374100d6f19f7e78f42bda1f51793a7d3b5bfc5
descouens-ptx <descouens-n@g.sys.pubtex.com>
2025-03-28 14:15:01 +0900

    feat: ass backport workflow (#1)

---
+++ scripts/backport.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Arguments
+PR_NUMBER="$1"
+SOURCE_BRANCH="$2"
+PR_TITLE="[Backport] $3"
+
+BACKPORT_BRANCH="backport-${SOURCE_BRANCH}-to-develop/pr-${PR_NUMBER}"
+PR_COMMITS=$(gh pr view $PR_NUMBER --json commits | jq -r '.commits[].oid')
+PR_BODY="Automated backport of #$PR_NUMBER to ${SOURCE_BRANCH}"
+
+echo "git switch -c $BACKPORT_BRANCH $SOURCE_BRANCH"
+git switch -c $BACKPORT_BRANCH $SOURCE_BRANCH
+
+for commit in $PR_COMMITS; do
+  echo "git cherry-pick $commit"
+  if ! git cherry-pick $commit; then
+    echo "Cannot cherry-pick commit $commit"
+    ERROR=true
+  fi
+done
+
+if [[ $ERROR == false ]]; then
+  git push origin $BACKPORT_BRANCH
+fi
